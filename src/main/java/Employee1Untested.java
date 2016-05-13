3682b4fd46fcf6c62e3031b999ab176e6c66464c
chandani.thacker <chandani.thacker@attuneww.com>
2016-05-13 15:55:02 +0530

    init comit

---
+++ src/main/java/Employee1Untested.java
@@ -0,0 +1,22 @@
+
+public class Employee1Untested {
+	public int getSalary(int numofDays,int dailySalary)
+	{
+		return numofDays*dailySalary;
+	}
+	
+	
+	public void newMethods()
+	{
+		System.out.println("Simple method");
+		
+	}
+	
+	
+	public void newMethods2()
+	{
+		System.out.println("Simple method  2");
+		
+	}
+
+}
